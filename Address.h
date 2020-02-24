6bd9f0ed38d5c4072928ca101f717155547945f9
mari954 <60939578+Mari954@users.noreply.github.com>
2020-02-24 17:03:44 -0500

    Add files via upload

---
+++ Address.h
@@ -0,0 +1,28 @@
+#include <string>
+
+#ifndef ADDRESS_H
+#define ADDRESS_H
+
+class Address
+{
+public:
+	Address(std::string, std::string, std::string, std::string);
+	void setStreetAddress(std::string);
+	void setCity(std::string);
+	void setState(std::string);
+	void setZipCode(std::string);
+
+	std::string getStreetAddress();
+	std::string getCity();
+	std::string getState();
+	std::string getZipCode();
+
+private:
+	std::string streetAddress;
+	std::string city;
+	std::string state;
+	std::string zipCode;
+
+}; // end class Address
+
+#endif ADDRESS_H
