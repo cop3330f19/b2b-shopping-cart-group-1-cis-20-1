25a397caad6540adbeee269dd800d5b432580855
mari954 <60939578+Mari954@users.noreply.github.com>
2020-02-28 11:17:49 -0500

    Update Product.cpp

--- Product.cpp
+++ Product.cpp
@@ -6,56 +6,54 @@
 #include <string>
 #include <iostream>
 #include "Product.h"
+
+
 using namespace std;
 
-    int itemNo;
-    std::string description;
-    double price;
-    int stockQuantity;
-    
-    
-    
-   Product::Product(){}
-    Product::Product(int itemNum, std::string itemDes, double itemPri, int itemQua)
-    {
-        itemNo = itemNum;
-        description = itemDes;
-        price = itemPri;
-         stockQuantity = itemQua;
-    };
-  
-    
-    int getItemNo(){return itemNo;}; 
-  
-std::string getDescription(){return description;};
-   
-    double getPrice(){return price;};
-   
-    int getStockQuantity(){return stockQuantity;};
-  
-    
-    void setItemNo(int itemNum){itemNo = itemNum;};
-   
-    void setDescription(std::string itemDes){description = itemDes;};
-  
-    void setPrice(double itemPri){price = itemPri;};
-    
-    void setStockQuantity(int itemQua){stockQuantity = itemQua;};
+Product::Product(int item_num, std::string item_des, double item_price, int item_stock)
+{
+	setProductNum(item_num);
+	setProductDes(item_des);
+	setProductPrice(item_price);
+	setProductStock(item_stock);
+}
 
+void Product::setProductNum(int item_num)
+{
+	itemNo = item_num;
+}
 
-      void const print()
-    {
-        cout << "\n--------------------------------------------" << endl;
-        cout << "Item Num" << setw(18) << "Description" << setw(10) << "Quantity" << setw(9) << "Total" << endl;
-        cout << "--------------------------------------------" << endl << endl;
-        
-        cout << itemNo << setw(22) << description << setw(7) << stockQuantity << setw(10) << setprecision(2) 
-            << showpoint << fixed << price * stockQuantity << endl << endl;
-        
-       cout << "--------------------------------------------" << endl;
-       cout << "Total" << setw(38) << setprecision(2) << fixed << showpoint 
-         <<  price * stockQuantity << endl;
-       cout << "--------------------------------------------" << endl;
-      
-    };
+void Product::setProductDes(string item_des)
+{
+	description = item_des;
+}
 
+void Product::setProductPrice(double item_price)
+{
+	price = item_price;
+}
+
+void Product::setProductStock(int item_stock)
+{
+	stockQuantity = item_stock;
+}
+
+int Product::getProductNum()
+{	
+	return itemNo;
+}
+
+string Product::getProductDes()
+{
+	return description;
+}
+
+double Product::getProductPrice()
+{
+	return price;
+}
+
+int Product::getProductStock()
+{
+	return stockQuantity;
+}
