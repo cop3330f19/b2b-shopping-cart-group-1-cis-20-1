b202494cebec3e7bbbb5e681e94a5f291c2eed23
rhiana1long <57336735+rhiana1long@users.noreply.github.com>
2020-02-27 23:29:45 -0500

    Update Product.cpp

--- Product.cpp
+++ Product.cpp
@@ -1,12 +1,10 @@
+
 #include <iomanip>
 #include <string>
 #include <iostream>
 #include "Product.h"
 using namespace std;
 
-class Product{
-  public:
-    
     int itemNo;
     std::string description;
     double price;
@@ -14,8 +12,8 @@ class Product{
     
     
     
-    Product();
-    Product(int itemNum, std::string itemDes, double itemPri, int itemQua)
+   Product::Product(){}
+    Product::Product(int itemNum, std::string itemDes, double itemPri, int itemQua)
     {
         itemNo = itemNum;
         description = itemDes;
@@ -24,9 +22,9 @@ class Product{
     };
   
     
-    int getItemNo(){return itemNo};
+    int getItemNo(){return itemNo;}; 
   
-    std::string getDescription(){return description;};
+std::string getDescription(){return description;};
    
     double getPrice(){return price;};
    
@@ -40,4 +38,21 @@ class Product{
     void setPrice(double itemPri){price = itemPri;};
     
     void setStockQuantity(int itemQua){stockQuantity = itemQua;};
-};
+
+
+      void const print()
+    {
+        cout << "\n--------------------------------------------" << endl;
+        cout << "Item Num" << setw(18) << "Description" << setw(10) << "Quantity" << setw(9) << "Total" << endl;
+        cout << "--------------------------------------------" << endl << endl;
+        
+        cout << itemNo << setw(22) << description << setw(7) << stockQuantity << setw(10) << setprecision(2) 
+            << showpoint << fixed << price * stockQuantity << endl << endl;
+        
+       cout << "--------------------------------------------" << endl;
+       cout << "Total" << setw(38) << setprecision(2) << fixed << showpoint 
+         <<  price * stockQuantity << endl;
+       cout << "--------------------------------------------" << endl;
+      
+    };
+
